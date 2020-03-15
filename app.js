2ac7cc5917b920e272a44248b7ea2040f6df3340
kacox1251 <56182547+kacox1251@users.noreply.github.com>
2020-03-14 19:50:00 -0600

    added inquirer

--- app.js
+++ app.js
@@ -5,23 +5,153 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const outputPath = path.resolve(__dirname, "output", "team.html");
+const outputPath = path.resolve(__dirname, "templates", "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-inquirer
-  .prompt([
-      { type: "input", name: "hello", message: "entersomehting" },
-      { type: "input" }
-])
-  .then(answers => {
-    console.log(answers);
-    // Use user feedback for... whatever!!
-  })
-  .catch(error => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
+let teamMembers = [];
+let employeeId = [];
+
+console.log("Welcome to the Team Builder. Please start by entering your team manager's information.");
+
+const validateId = (data) => {
+  if (isNaN(data)) {
+      return "Employee ID's should be 4-digits. Please re-enter your employee's ID.";
+  }
+  for (i = 0; i < employeeId.length; i++) {
+      if (data === employeeId[i]) {
+          return "ID number in use. Please enter different ID."
+      }
+  }
+  return true;
+}
+
+const generateEmployees = function() {
+    inquirer.prompt([
+        {
+        type: "input",
+        message: "Enter team manager's name:",
+        name: "name"
+        },
+        {
+        type: "number",
+        message: "Enter their 4-digit ID number:",
+        name: "id",
+        validate: validateId
+        },
+        {
+        type: "input",
+        message: "Enter email address:",
+        name: "email"
+        },
+        {
+        type: "number",
+        message: "Enter office phone number:",
+        name: "officeNumber"
+        },
+    ]).then((response) => {
+
+        const newManager = new Manager(response.name, response.id, response.email, response.officeNumber);
+
+        teamMembers.push(newManager);
+        employeeId.push(response.id);
+
+        addEmployees();
+    });
+};
+
+const engineer = function () {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter engineer's name:",
+      name: "name"
+    },
+    {
+      type: "number",
+      message: "Enter their 4-digit ID number:",
+      name: "id",
+      validate: validateId
+    },
+    {
+      type: "input",
+      message: "Enter email address:",
+      name: "email"
+    },
+    {
+      type: "input",
+      message: "Enter GitHub user name:",
+      name: "github"
+    }
+  ]).then(function(response) {
+    const newEngineer = new Engineer(response.name, response.id, response.email, response.github);
+
+    teamMembers.push(newEngineer);
+    employeeId.push(response.id);
+
+    addEmployees();
+  });
+};
+
+const intern = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter Intern's name:",
+      name: "name"
+    },
+    {
+      type: "number",
+      message: "Interns's 4-digit ID number:",
+      name: "id",
+      validate: validateId
+    },
+    {
+      type: "input",
+      message: "Interns's email address:",
+      name: "email"
+    },
+    {
+      type: "input",
+      message: "Where is your intern attending school?",
+      name: "school"
+    }
+  ]).then((response) => {
+    const newIntern = new Intern(response.name, response.id, response.email, response.school);
+
+    teamMembers.push(newIntern);
+    employeeId.push(response.id);
+
+    addEmployees();
+  });
+};
+
+const addEmployees = () => {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "Would you like to add another employee?",
+      choices: ["Engineer", "Intern", "No thanks, my team is complete."],
+      name: "newEmployee"
+    }
+  ]).then((response) => {
+    switch (response.newEmployee) {
+      case "Engineer":
+        engineer();
+        break;
+      case "Intern":
+        intern();
+        break;
+      default:
+        fs.writeFile(outputPath, render(teamMembers), function (err) {
+          if (err) {
+            throw err;
+          }
+        });
+        console.log("Your team page has been created!");
+        break;
     }
   });
+};
+
+generateEmployees();
